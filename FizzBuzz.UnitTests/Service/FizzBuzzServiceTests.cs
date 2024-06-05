92471fc93da0351f03dceb13b59be7ccbf8267e7
Rahulbabu Bunga <rahulbabubunga@allianzlife.com>
2024-06-05 18:15:26 -0500

    Added test project

---
+++ FizzBuzz.UnitTests/Service/FizzBuzzServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using FizzBuzz.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.UnitTests.Service
+{
+    public class FizzBuzzServiceTests
+    {
+        private readonly FizzBuzzService _sut;
+
+        public FizzBuzzServiceTests()
+        {
+            _sut = new FizzBuzzService();
+        }
+
+        [Fact]
+        public void ItShouldReturnFizz_IfInputIsDivisibleBy3()
+        {
+            var result = _sut.FetchResult("3");
+
+            Assert.Equal("Fizz", result.Results[0]);
+        }
+
+        [Fact]
+        public void ItShouldReturnFizz_IfInputIsDivisibleBy5()
+        {
+            var result = _sut.FetchResult("5");
+
+            Assert.Equal("Buzz", result.Results[0]);
+        }
+
+    }
+}
--- FizzBuzz/Service/FizzBuzzService.cs
+++ FizzBuzz/Service/FizzBuzzService.cs
@@ -9,43 +9,48 @@ namespace FizzBuzz.Service
             var responses = new List<Response>();
             foreach (var input in inputs)
             {
-
-                //handle invalid case upfront
-                int i = 0;
-                if (string.IsNullOrWhiteSpace(input) || !int.TryParse(input, out i))
-                {
-                    responses.Add(new Response(input, new List<string>() { "Invalid Item" }));
-                }
-
-                else
-                {
-                    bool divisibleBy3, divisibleBy5;
-                    divisibleBy3 = divisibleBy5 = false;
-
-                    if (i % 3 == 0)
-                        divisibleBy3 = true;
-                    if (i % 5 == 0)
-                        divisibleBy5 = true;
-
-                    if (divisibleBy3 && divisibleBy5)
-                    {
-                        responses.Add(new Response(input, new List<string>() { "FizzBuzz" }));
-                    }
-                    else if (divisibleBy3)
-                    {
-                        responses.Add(new Response(input, new List<string>() { "Fizz" }));
-                    }
-                    else if (divisibleBy5)
-                    {
-                        responses.Add(new Response(input, new List<string>() { "Buzz" }));
-                    }
-                    else
-                        responses.Add(new Response(input, new List<string>() { $"Divided {input} by 3", $"Divided {input} by 5" }));
-                }
-
+                responses.Add(FetchResult(input));
 
             }
             return Task.FromResult(responses);
+        }
+
+        public Response FetchResult(string input)
+        {
+            //handle invalid case upfront
+            int i = 0;
+            if (string.IsNullOrWhiteSpace(input) || !int.TryParse(input, out i))
+            {
+                return new Response(input, new List<string>() { "Invalid Item" });
+            }
+
+            else
+            {
+                bool divisibleBy3, divisibleBy5;
+                divisibleBy3 = divisibleBy5 = false;
+
+                if (i % 3 == 0)
+                    divisibleBy3 = true;
+                if (i % 5 == 0)
+                    divisibleBy5 = true;
+
+                if (divisibleBy3 && divisibleBy5)
+                {
+                    return new Response(input, new List<string>() { "FizzBuzz" });
+                }
+                else if (divisibleBy3)
+                {
+                    return new Response(input, new List<string>() { "Fizz" });
+                }
+                else if (divisibleBy5)
+                {
+                    return new Response(input, new List<string>() { "Buzz" });
+                }
+                else
+                {
+                    return new Response(input, new List<string>() { $"Divided {input} by 3", $"Divided {input} by 5" });
+                }
+            }
         }
     }
 }
